ea7797d9abe3735ce4aae68575fb77187492422c
Itamar Mizrahi <mizrahi.itamar@gmail.com>
2025-08-21 18:06:35 +0300

    Make static export optional

--- realestate-broker-ui/next.config.mjs
+++ realestate-broker-ui/next.config.mjs
@@ -1,3 +1,12 @@
+const isStaticExport = process.env.NEXT_STATIC_EXPORT === 'true'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, experimental: { serverActions: { allowedOrigins: ['*'] } } }
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: { allowedOrigins: ['*'] },
+  },
+  ...(isStaticExport && { images: { unoptimized: true }, output: 'export' }),
+}
+
 export default nextConfig

cc582114d30825b311ac19de080d7b8a84e35711
Itamar Mizrahi <mizrahi.itamar@gmail.com>
2025-09-24 12:04:12 +0300

    Remove duplicate RamiCollector implementation

--- gov/rami/__init__.py
+++ gov/rami/__init__.py
@@ -1,6 +1,5 @@
 """RAMI (Israel Land Authority) Taba data client."""
 
-from .collector import RamiCollector
 from .rami_client import RamiClient
 
-__all__ = ["RamiClient", "RamiCollector"]
+__all__ = ["RamiClient"]

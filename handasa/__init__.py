e9b2e4254fccba2d4baa24d92f529dace545ec74
Itamar Mizrahi <mizrahi.itamar@gmail.com>
2025-10-12 15:33:59 +0300

    Update __init__.py

--- handasa/__init__.py
+++ handasa/__init__.py
@@ -1,8 +1,1 @@
-"""Utilities for scraping permits from the Tel Aviv Handasa portal."""
 
-from .scraper import HandasaScraper, HandasaScraperError
-
-__all__ = [
-    "HandasaScraper",
-    "HandasaScraperError",
-]

18caa55bcf708041ef9dca88e662f0b31957bcd8
Itamar Mizrahi <mizrahi.itamar@gmail.com>
2025-10-25 21:00:47 +0300

    Allow Next Image from img.yad2.co.il

--- realestate-broker-ui/next.config.mjs
+++ realestate-broker-ui/next.config.mjs
@@ -32,6 +32,7 @@ const nextConfig = {
       { protocol: "https", hostname: "api.nadlaner.com" },
       { protocol: "https", hostname: "backend-django-2cpe.onrender.com" },
       { protocol: "https", hostname: "cdn.yad2.treedis.com" },
+      { protocol: "https", hostname: "img.yad2.co.il" },
       { protocol: "http", hostname: "127.0.0.1" },
       { protocol: "http", hostname: "localhost" },
     ],

d95983d40f63248741756b213e77ebb3b873bd1b
Itamar Mizrahi <mizrahi.itamar@gmail.com>
2025-10-26 13:27:29 +0200

    .

---
+++ backend-django/core/migrations/0048_merge_0045_rename_core_asset_block_dc43e9_idx_core_asset_block_531195_idx_and_more_0047_add_listing_ad_type.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0045_rename_core_asset_block_dc43e9_idx_core_asset_block_531195_idx_and_more"),
+        ("core", "0047_add_listing_ad_type"),
+    ]
+
+    operations = []

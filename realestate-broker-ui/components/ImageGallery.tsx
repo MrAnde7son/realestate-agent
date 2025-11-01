8930694fa779a3ed0bf9e056a74998fae0ce3167
Itamar Mizrahi <mizrahi.itamar@gmail.com>
2025-11-01 19:29:13 +0200

    Update ImageGallery.tsx

--- realestate-broker-ui/components/ImageGallery.tsx
+++ realestate-broker-ui/components/ImageGallery.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react'
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
@@ -145,9 +144,6 @@ export default function ImageGallery({
                 <X className="h-4 w-4" />
               </Button>
             </div>
-            <DialogDescription className="sr-only">
-              גלריית תמונות בתצוגה מלאה. השתמשו בכפתורי הניווט למעבר בין תמונות ובכפתור הסגירה לחזרה לרשימת התמונות.
-            </DialogDescription>
           </DialogHeader>
           
           <div className="relative flex-1 p-2 sm:p-4">

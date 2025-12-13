397e28c5a6a7df116418f4ba66acd89bee9f4476
Itamar Mizrahi <mizrahi.itamar@gmail.com>
2025-12-13 21:47:37 +0200

    chore: fix sidebar

--- realestate-broker-ui/components/layout/app-sidebar.tsx
+++ realestate-broker-ui/components/layout/app-sidebar.tsx
@@ -142,7 +142,7 @@ export default function AppSidebar({
     >
 
       {/* Navigation */}
-      <div className="flex-1 overflow-y-auto p-3">
+      <div className="flex-1 overflow-y-auto p-3 pt-16">
         <Tooltip.Provider delayDuration={0} skipDelayDuration={0}>
           <nav className="space-y-2">
             {navigation.map((item) => {
